d5096781d4bf768cd2d5a2228204bcab2faa3e69
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-11 00:11:54 -0800

    Fix resizing exceeding borders bug
    Fix issue where the pixels can spill past the parent
    grid container.

--- script.js
+++ script.js
@@ -6,12 +6,26 @@ https://developer.mozilla.org/en-US/docs/Web/API/Resize_Observer_API
 drawGrid();
 
 const MAXGRID = 100;
-let dimUserSelected = 1;
+let gridDim = 16;
+
+const resizeButton = document.querySelector(`.resize-button`);
+resizeButton.addEventListener(`click`, () => {
+  let userSelectedDim = Number(prompt(`Enter Grid Dimension.`,`16`));
+  while (isNaN(userSelectedDim)) {
+    const errorMsg = document.createElement(`div`);
+    errorMsg.textContent = `Please enter a valid number.`
+    userSelectedDim = Number(prompt(`Enter Grid Dimension.`,`16`));
+  }
+  gridDim = Number(userSelectedDim);
+  const gridContainer = document.querySelector(`.grid-container`);
+  deletePixels();
+  drawGrid(gridDim);
+})
 
 const resizeObserver = new ResizeObserver((entries) => {
   for (const entry of entries) {
     if (entry.contentRect) {
-      resizePixels(entry.contentRect.width);
+      resizePixels(entry.contentRect.width, gridDim);
     }
   }
 })
@@ -21,11 +35,16 @@ resizeObserver.observe(document.querySelector(`.grid-container`));
 function resizePixels(containerDim = 1, dim = 16) {
   const pixels = document.querySelectorAll(`.pixel`);
   let dimPixel = 1;
-  dimPixel = Math.floor(containerDim/dim);
+  dimPixel = Math.floor(Math.floor(containerDim)/dim);
   pixels.forEach((pixel) => { 
     pixel.style.height = dimPixel + `px`;
     pixel.style.width = dimPixel + `px`;
   })
+}
+
+function deletePixels() {
+  const gridContainer = document.querySelector(`.grid-container`);
+  gridContainer.replaceChildren([])
 }
 
 function drawGrid(dim = 16){
@@ -39,6 +58,7 @@ function drawGrid(dim = 16){
       const pixel = document.createElement(`div`);
       pixel.style.borderRight = `1px solid #eeeeee`;
       pixel.style.borderBottom = `1px solid #eeeeee`;
+      pixel.style.boxSizing = `border-box`;
       pixel.classList.add(`pixel`);
       pixel.addEventListener('mouseenter', () => {
         pixel.style.backgroundColor = `#333333`;
@@ -46,5 +66,5 @@ function drawGrid(dim = 16){
       pixelRow.appendChild(pixel);
     }
   }
-  resizePixels(gridContainer.clientWidth);
+  resizePixels(gridContainer.clientWidth, dim);
 }

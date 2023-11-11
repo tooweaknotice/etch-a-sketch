a53671cb69d170cb77518672ff9727a4c971f7e3
fashionpolice <60684862+tooweaknotice@users.noreply.github.com>
2023-11-10 23:25:41 -0800

    Add dynamic grid resizing to window
    Grid will resize as window resizes

--- script.js
+++ script.js
@@ -7,17 +7,16 @@ drawGrid();
 
 const MAXGRID = 100;
 let dimUserSelected = 1;
-/*
+
 const resizeObserver = new ResizeObserver((entries) => {
   for (const entry of entries) {
-    if (entry.contentBoxSize) {
-      const contentBoxSize = entry.contentBoxSize[0];
-      resizePixels(contentBoxSize.width);
+    if (entry.contentRect) {
+      resizePixels(entry.contentRect.width);
     }
   }
 })
 
-resizeObserver.observe(document.querySelector(".grid-container"));
+resizeObserver.observe(document.querySelector(`.grid-container`));
 
 function resizePixels(containerDim = 1, dim = 16) {
   const pixels = document.querySelectorAll(`.pixel`);
@@ -28,23 +27,18 @@ function resizePixels(containerDim = 1, dim = 16) {
     pixel.style.width = dimPixel + `px`;
   })
 }
-*/
 
 function drawGrid(dim = 16){
+  const gridContainer = document.querySelector(`.grid-container`);
   for (let i = 0; i < dim; i++){
-    const gridContainer = document.querySelector(`.grid-container`);
     const pixelRow = document.createElement(`div`);
     pixelRow.style.display = `flex`;
     pixelRow.classList.add(`pixel-row`)
     gridContainer.appendChild(pixelRow);
     for (let j = 0; j < dim; j++){
-      let dimPixel = 1;
-      dimPixel = Math.floor(gridContainer.clientWidth/dim);
       const pixel = document.createElement(`div`);
       pixel.style.borderRight = `1px solid #eeeeee`;
       pixel.style.borderBottom = `1px solid #eeeeee`;
-      pixel.style.height = dimPixel + `px`;
-      pixel.style.width = dimPixel + `px`;
       pixel.classList.add(`pixel`);
       pixel.addEventListener('mouseenter', () => {
         pixel.style.backgroundColor = `#333333`;
@@ -52,4 +46,5 @@ function drawGrid(dim = 16){
       pixelRow.appendChild(pixel);
     }
   }
+  resizePixels(gridContainer.clientWidth);
 }
